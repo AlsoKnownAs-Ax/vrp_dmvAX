9d85008069c5bc048e43c1336c9bc71af78e66bb
AlsoKnownAs-Ax <92175683+AlsoKnownAs-Ax@users.noreply.github.com>
2022-08-19 22:43:40 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,9 +3,11 @@ AxConfig = {}
 
 AxConfig.DriverLicenseItem = "permisc" --[[ Here you set your drivers license name ex: permis ]]
 
-AxConfig.UseOkOkNotify = false -- [[ Set this on false if you don`t use okoknotify ]]
+AxConfig.UseOkOkNotify = true -- [[ Set this on false if you don`t use okoknotify ]]
 
 AxConfig.DmvCarId = "brioso" --[[ Here you can put your exam car spawn id, ex (from GTA 5 cars) : brioso ]]
+
+AxConfig.VirtualWolrd = true --[[ Set this to false if you don`t want the players that start the test drive to be set in virtual world ]]
 
 
 --[[ Locatia din sectie de unde incepi testul auto se schimba la liniile 36,37,38,40 din server.lua ]]
